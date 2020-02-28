c354350b9c2d8b18b9bc455fae19e86642b8d138
Vinay Bharadhwaj <vinaysbharadhwaj@gmail.com>
2020-02-28 14:00:44 +0100

    Added shell script to get custom files

---
+++ get_custom.sh
@@ -0,0 +1,12 @@
+cd ~/.oh-my-zsh/custom/plugins/
+
+git clone https://github.com/MichaelAquilina/zsh-autoswitch-virtualenv.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/Valiev/almostontop.git
+
+cd ~/.oh-my-zsh/custom/themes/
+
+git clone https://github.com/romkatv/powerlevel10k.git
+git clone https://github.com/Powerlevel9k/powerlevel9k.git
+
+cd ~/
